6d4d8000f2c2c737ab170b019e2df9f63b9ac8c5
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-02-06 19:07:45 -0300

    feat: insere rotas e middleware de erro

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
 import express from 'express';
+import HttpError from './Middlewares/http.error.middleware';
+import routes from './Routes/router';
 
 const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+
+app.use(HttpError.handle);
 
 export default app;
