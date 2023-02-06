3547a3393ce5c749c7ddf337300343bbb565ba08
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-02-06 20:39:38 -0300

    feat: Abstract ODM para criar na ORM cars

---
+++ src/Models/AbstractODM.ts
@@ -0,0 +1,15 @@
+import { Model, model, models, Schema } from 'mongoose';
+
+export default abstract class AbstractODM<T> {
+  protected model: Model<T>;
+  protected schema: Schema;
+  protected modelName: string;
+
+  constructor(schema: Schema, modelName: string) {
+    this.schema = schema;
+    this.modelName = modelName;
+    this.model = models[this.modelName] || model(this.modelName, this.schema);
+  }
+
+  public create = async (car: T): Promise<T> => this.model.create({ ...car });
+}
