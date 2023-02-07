d65b735390301bf10f32227529ccd9815b599e83
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-02-06 22:22:57 -0300

    feat: método delete para rota /cars/id

--- src/Routes/CarsRoutes.ts
+++ src/Routes/CarsRoutes.ts
@@ -15,4 +15,7 @@ router.get('/:id', (req, res, next) =>
 router.put('/:id', (req, res, next) =>
   new CarsController(req, res, next).updateById());
 
+router.delete('/:id', (req, res, next) =>
+  new CarsController(req, res, next).deleteById());
+
 export default router;
