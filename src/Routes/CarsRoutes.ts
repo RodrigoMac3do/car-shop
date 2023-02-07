379ad61cd196e737127a7a478d56cd60334dd46b
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-02-06 21:37:02 -0300

    feat: método get para listar tudo e listar por id

--- src/Routes/CarsRoutes.ts
+++ src/Routes/CarsRoutes.ts
@@ -6,4 +6,10 @@ const router = Router();
 router.post('/', (req, res, next) =>
   new CarsController(req, res, next).create());
 
+router.get('/', (req, res, next) =>
+  new CarsController(req, res, next).findAll());
+
+router.get('/:id', (req, res, next) =>
+  new CarsController(req, res, next).findById());
+
 export default router;
