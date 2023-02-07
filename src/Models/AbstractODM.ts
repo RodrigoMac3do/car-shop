49d520a6b629be0aaff2c5f2b9bee8fef0f5b820
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-02-06 22:14:39 -0300

    feat: ODM para atualizar carro por id

--- src/Models/AbstractODM.ts
+++ src/Models/AbstractODM.ts
@@ -1,4 +1,5 @@
 import { Model, model, models, Schema } from 'mongoose';
+import ICar from '../Interfaces/ICar';
 
 export default abstract class AbstractODM<T> {
   protected model: Model<T>;
@@ -11,10 +12,19 @@ export default abstract class AbstractODM<T> {
     this.model = models[this.modelName] || model(this.modelName, this.schema);
   }
 
-  public create = async (car: T): Promise<T> => this.model.create({ ...car });
+  public create = async (car: ICar): Promise<T> => this.model.create({ ...car });
 
   public findAll = async (): Promise<T[]> => this.model.find();
 
   public findById = async (id: string): Promise<T | null> =>
     this.model.findById(id);
+
+  public updateById = async (
+    id: string,
+    car: ICar,
+  ): Promise<T | null> => {
+    const carUpdated = await this.model.findByIdAndUpdate(id, car);
+
+    return carUpdated;
+  };
 }
