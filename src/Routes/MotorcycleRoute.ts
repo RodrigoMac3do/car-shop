4a7a687303f3d60fcb8453a984858c6cd3231abc
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-02-06 23:46:37 -0300

    feat: método delete da rota /motorcycles

--- src/Routes/MotorcycleRoute.ts
+++ src/Routes/MotorcycleRoute.ts
@@ -15,4 +15,7 @@ router.get('/:id', (req, res, next) =>
 router.put('/:id', (req, res, next) =>
   new MotorcycleController(req, res, next).updateById());
 
+router.delete('/:id', (req, res, next) =>
+  new MotorcycleController(req, res, next).deleteById());
+
 export default router;
