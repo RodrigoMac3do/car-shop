a35b12d249927b2ae034cd662165a4b155ff9264
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-02-06 23:45:13 -0300

    feat: método get da rota /motorcycles

--- src/Routes/MotorcycleRoute.ts
+++ src/Routes/MotorcycleRoute.ts
@@ -6,4 +6,10 @@ const router = Router();
 router.post('/', (req, res, next) =>
   new MotorcycleController(req, res, next).create());
 
+router.get('/', (req, res, next) =>
+  new MotorcycleController(req, res, next).findAll());
+
+router.get('/:id', (req, res, next) =>
+  new MotorcycleController(req, res, next).findById());
+
 export default router;
