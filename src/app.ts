402528766e049178d88bc37bb650f0381ea4ffc5
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-02-08 18:13:13 -0300

    feat: import de express-async-errors

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import express from 'express';
-// import 'express-async-errors';
-import HttpError from './Middlewares/http.error.middleware';
-import routes from './Routes/router';
+import 'express-async-errors';
+import HttpError from './Middlewares/HttpError.middleware';
+import Routes from './Routes/Router';
 
 const app = express();
 
 app.use(express.json());
 
-app.use(routes);
+app.use(Routes);
 
 app.use(HttpError.handle);
 
