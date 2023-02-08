214742d55639cd2a9100d1f3c388ff057b2d85c1
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-02-08 18:08:06 -0300

    style: exclui linhas comentadas

--- src/Utils/ValidateObjectId.ts
+++ src/Utils/ValidateObjectId.ts
@@ -1,11 +1,7 @@
 import { isValidObjectId } from 'mongoose';
-// import HttpException from './http.exception';
 
 export default class ValidateObjectId {
-  // public validate(id: string): void {
-  //   if (!isValidObjectId(id)) throw new HttpException(422, 'Invalid mongo id');
-  // }
-  public validate(id: string): void {
+  public validate = (id: string): void => {
     if (!isValidObjectId(id)) throw new Error('Invalid mongo id');
-  }
+  };
 }
