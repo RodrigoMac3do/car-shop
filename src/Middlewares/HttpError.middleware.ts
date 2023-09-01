7503f90665c8694d633a14224c3612d0003d6044
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-08-31 23:57:58 -0300

    refactor: remove outros tipos de exceção

--- src/Middlewares/HttpError.middleware.ts
+++ src/Middlewares/HttpError.middleware.ts
@@ -5,14 +5,6 @@ export default class HttpError {
   public static handle: ErrorRequestHandler = (err, _req, res, next) => {
     const { status, message } = err as HttpException;
 
-    if (message.includes('not found')) {
-      res.status(404).json({ message });
-    }
-
-    if (message.includes('Invalid')) {
-      res.status(422).json({ message });
-    }
-
     res.status(status || 500).json({ message });
 
     next();
