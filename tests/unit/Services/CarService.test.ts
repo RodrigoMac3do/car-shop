66f91d42e835dc9bf0eb0d2b2358bfb6d00421ba
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-09-04 09:57:30 -0300

    fix: Correção de testes

--- tests/unit/Services/CarService.test.ts
+++ tests/unit/Services/CarService.test.ts
@@ -1,18 +1,23 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
-import { Model } from 'mongoose';
+import mongoose from 'mongoose';
 import CarService from '../../../src/Services/CarService';
 import {
   car,
   cars,
+  carUpdate,
   carWithoutStatus,
   carWithoutStatusOut,
 } from '../../mocks/data';
 
 describe('Testes da CarService', function () {
+  beforeEach(function () {
+    sinon.restore();
+  });
+
   describe('Testes com POST da rota /cars', function () {
     it('Teste de criação de novo carro com sucesso', async function () {
-      sinon.stub(Model, 'create').resolves(cars[0]);
+      sinon.stub(mongoose.Model, 'create').resolves(cars[0]);
 
       const service = new CarService();
 
@@ -22,7 +27,7 @@ describe('Testes da CarService', function () {
     });
 
     it('Teste de criação de novo carro sem status', async function () {
-      sinon.stub(Model, 'create').resolves(carWithoutStatusOut);
+      sinon.stub(mongoose.Model, 'create').resolves(carWithoutStatusOut);
 
       const service = new CarService();
 
@@ -32,7 +37,7 @@ describe('Testes da CarService', function () {
     });
 
     it('Teste de criação de noco carro sem propriedade', async function () {
-      sinon.stub(Model, 'create').resolves(undefined);
+      sinon.stub(mongoose.Model, 'create').resolves(undefined);
 
       const service = new CarService();
 
@@ -44,7 +49,7 @@ describe('Testes da CarService', function () {
 
   describe('Testes com GET da rota /cars', function () {
     it('Teste deve retornar lista de carros', async function () {
-      sinon.stub(Model, 'find').resolves(cars);
+      sinon.stub(mongoose.Model, 'find').resolves(cars);
 
       const service = new CarService();
 
@@ -53,44 +58,41 @@ describe('Testes da CarService', function () {
       expect(result).to.be.deep.equal(cars);
     });
 
-    it('Teste de falha ao passar ID inválido', async function () {
-      sinon.stub(Model, 'findById').resolves(cars[0]);
+    it('Teste de falha ao passar ID inválidoo', async function () {
+      sinon.stub(mongoose.Model, 'findById').resolves(null);
 
       try {
         const service = new CarService();
 
-        await service.findById('123121231werwrwe');
+        await service.findById('1');
       } catch (error) {
-        expect((error as Error).message).to.be.equal('Invalid mongo id');
+        expect((error as Error).message).to.be.equal('Car not found');
       }
     });
   });
 
   describe('Testes com PUT da rota /cars', function () {
-    // it('Teste se é possível alterar um carro com sucesso', async function () {
-    //   sinon.stub(Model, 'findByIdAndUpdate').resolves(cars[0]);
-    //   const service = new CarService();
-    //   const result = await service.updateById(
-    //     '63e2aae4135a458473a32730',
-    //     cars[1],
-    //   );
-    //   expect(result).to.be.deep.equal(cars[1]);
-    // });
-    // it('Teste deve falhar ao tentar atualizar um carro inexistente', async function () {
-    //   sinon.stub(Model, 'findByIdAndUpdate').resolves(null);
-    //   try {
-    //     const service = new CarService();
-    //     await service.updateById('63e2aae4135a458473a32722', cars[1]);
-    //   } catch (error) {
-    //     expect((error as Error).message).to.be.equal('Car not found');
-    //   }
-    // });
+    it('Deve atualizar um carro', async function () {
+      sinon.stub(mongoose.Model, 'findById').resolves(cars[0]);
+      sinon.stub(mongoose.Model, 'findByIdAndUpdate').resolves(carUpdate);
+
+      const service = new CarService();
+      const result = await service.updateById(
+        '63e2aae4135a458473a32730',
+        carUpdate,
+      );
+
+      expect(result).to.be.deep.equal({
+        id: cars[0].id,
+        ...carUpdate,
+      });
+    });
   });
 
   describe('Teste com DELETE na rota /cars', function () {
     it('Teste se é possível excluir carro com sucesso', async function () {
-      sinon.stub(Model, 'findById').resolves(cars[0]);
-      sinon.stub(Model, 'findByIdAndRemove').resolves(null);
+      sinon.stub(mongoose.Model, 'findById').resolves(cars[0]);
+      sinon.stub(mongoose.Model, 'findByIdAndRemove').resolves(null);
 
       const service = new CarService();
 
@@ -98,21 +100,5 @@ describe('Testes da CarService', function () {
 
       expect(result).to.be.deep.equal(undefined);
     });
-
-    // it('Teste de falha ao tentar excluir carro ID errado', async function () {
-    //   sinon.stub(Model, 'findById').resolves(cars[0]);
-
-    //   try {
-    //     const service = new CarService();
-
-    //     await service.deleteById('63e1a7b2ae2cea3d8dcc32b8');
-    //   } catch (error) {
-    //     expect((error as Error).message).to.be.deep.equal('Car not Found');
-    //   }
-    // });
-  });
-
-  afterEach(function () {
-    sinon.restore();
   });
 });
--- tests/unit/Services/MotorcycleService.test.ts
+++ tests/unit/Services/MotorcycleService.test.ts
@@ -1,18 +1,22 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
-import { Model } from 'mongoose';
+import mongoose from 'mongoose';
 import MotorcycleService from '../../../src/Services/MotorcycleService';
 import {
   motorcycle,
   motorcycles,
+  motorcycleUpdate,
   motorcycleWithoutStatus,
   motorcycleWithoutStatusOut,
 } from '../../mocks/data';
 
 describe('Testes da MotorcycleService', function () {
+  beforeEach(function () {
+    sinon.restore();
+  });
   describe('Testes com POST da rota /motorcycles', function () {
     it('Teste de criação de nova moto com sucesso', async function () {
-      sinon.stub(Model, 'create').resolves(motorcycles[0]);
+      sinon.stub(mongoose.Model, 'create').resolves(motorcycles[0]);
 
       const service = new MotorcycleService();
 
@@ -22,7 +26,7 @@ describe('Testes da MotorcycleService', function () {
     });
 
     it('Teste de criação de nova moto sem status', async function () {
-      sinon.stub(Model, 'create').resolves(motorcycleWithoutStatusOut);
+      sinon.stub(mongoose.Model, 'create').resolves(motorcycleWithoutStatusOut);
 
       const service = new MotorcycleService();
 
@@ -32,7 +36,7 @@ describe('Testes da MotorcycleService', function () {
     });
 
     it('Teste de criação de nova moto sem propriedade', async function () {
-      sinon.stub(Model, 'create').resolves(undefined);
+      sinon.stub(mongoose.Model, 'create').resolves(undefined);
 
       const service = new MotorcycleService();
 
@@ -44,7 +48,7 @@ describe('Testes da MotorcycleService', function () {
 
   describe('Testes com GET da rota /motorcycles', function () {
     it('Teste deve retornar lista de motos', async function () {
-      sinon.stub(Model, 'find').resolves(motorcycles);
+      sinon.stub(mongoose.Model, 'find').resolves(motorcycles);
 
       const service = new MotorcycleService();
 
@@ -54,43 +58,42 @@ describe('Testes da MotorcycleService', function () {
     });
 
     it('Teste de falha ao passar ID inválido', async function () {
-      sinon.stub(Model, 'findById').resolves(motorcycles[0]);
+      sinon.stub(mongoose.Model, 'findById').resolves(null);
 
       try {
         const service = new MotorcycleService();
 
-        await service.findById('63e2b814135a458473a3275q');
+        await service.findById('1');
       } catch (error) {
-        expect((error as Error).message).to.be.equal('Invalid mongo id');
+        expect((error as Error).message).to.be.equal('Motorcycle not found');
       }
     });
   });
 
   describe('Testes com PUT da rota /motorcycles', function () {
-    // it('Teste se é possível alterar um carro com sucesso', async function () {
-    //   sinon.stub(Model, 'findByIdAndUpdate').resolves(cars[0]);
-    //   const service = new MotorcycleService();
-    //   const result = await service.updateById(
-    //     '63e2aae4135a458473a32730',
-    //     motorcycles[1],
-    //   );
-    //   expect(result).to.be.deep.equal(motorcycles[1]);
-    // });
-    // it('Teste deve falhar ao tentar atualizar um carro inexistente', async function () {
-    //   sinon.stub(Model, 'findByIdAndUpdate').resolves(null);
-    //   try {
-    //     const service = new MotorcycleService();
-    //     await service.updateById('63e2aae4135a458473a32722', motorcycles[1]);
-    //   } catch (error) {
-    //     expect((error as Error).message).to.be.equal('Motorcycle not found');
-    //   }
-    // });
+    it('Deve atualizar um carro', async function () {
+      sinon.stub(mongoose.Model, 'findById').resolves(motorcycles[0]);
+      sinon
+        .stub(mongoose.Model, 'findByIdAndUpdate')
+        .resolves(motorcycleUpdate);
+
+      const service = new MotorcycleService();
+      const result = await service.updateById(
+        '63e2adb7135a458473a3274b',
+        motorcycleUpdate,
+      );
+
+      expect(result).to.be.deep.equal({
+        id: motorcycles[0].id,
+        ...motorcycleUpdate,
+      });
+    });
   });
 
   describe('Teste com DELETE na rota /motorcycles', function () {
     it('Teste se é possível excluir moto com sucesso', async function () {
-      sinon.stub(Model, 'findById').resolves(motorcycles[0]);
-      sinon.stub(Model, 'findByIdAndRemove').resolves(null);
+      sinon.stub(mongoose.Model, 'findById').resolves(motorcycles[0]);
+      sinon.stub(mongoose.Model, 'findByIdAndRemove').resolves(null);
 
       const service = new MotorcycleService();
 
@@ -98,21 +101,5 @@ describe('Testes da MotorcycleService', function () {
 
       expect(result).to.be.deep.equal(undefined);
     });
-
-    // it('Teste de falha ao tentar excluir uma moto com ID errado', async function () {
-    //   sinon.stub(Model, 'findById').resolves(motorcycles[0]);
-
-    //   try {
-    //     const service = new MotorcycleService();
-
-    //     await service.deleteById('63e1a7b2ae2cea3d8dcc32b8');
-    //   } catch (error) {
-    //     expect((error as Error).message).to.be.deep.equal('Motorcycle not Found');
-    //   }
-    // });
-  });
-
-  afterEach(function () {
-    sinon.restore();
   });
 });
